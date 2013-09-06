aa57312f8f62075bc88ae901851abde4565dda20
Pete Hollobon <phollobon@renshawbay.com>
2013-09-06 14:02:39 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Setup for SQLAlchemy backend for MonetDB
+"""
+from rb.setuptools import find_packages, setup
+
+setup_params = dict(
+    name="sqlalchemy_monetdb",
+    description="SQLAlchemy backend for MonetDB",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "sqlalchemy.dialects" : ["monetdb = sqlalchemy_monetdb:dialect"]
+    },
+    license="MIT",
+)
+
+if __name__ == '__main__':
+    setup(**setup_params)
