e2c06c3ca5ba43ee12b1e506a9b5ca66e58431dd
Gijs Molenaar <gijs@pythonic.nl>
2013-09-06 16:23:59 +0200

    added a bit more info

--- setup.py
+++ setup.py
@@ -2,12 +2,29 @@
 Setup for SQLAlchemy backend for MonetDB
 """
 from setuptools import find_packages, setup
+import os
+readme = os.path.join(os.path.dirname(__file__), 'README.md')
+
 
 setup_params = dict(
     name="sqlalchemy_monetdb",
-    description="SQLAlchemy backend for MonetDB",
+    description="SQLAlchemy dialect for MonetDB",
+    long_description=open(readme).read(),
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: Implementation :: CPython',
+      'Programming Language :: Python :: Implementation :: PyPy',
+      'Topic :: Database :: Front-Ends',
+      ],
+    keywords='MonetDB  SQLAlchemy',
     packages=find_packages(),
     include_package_data=True,
+    tests_require=['nose >= 0.11'],
+    test_suite="nose.collector",
     zip_safe=False,
     entry_points={
         "sqlalchemy.dialects": ["monetdb = sqlalchemy_monetdb:dialect"]
@@ -17,3 +34,4 @@ setup_params = dict(
 
 if __name__ == '__main__':
     setup(**setup_params)
+
