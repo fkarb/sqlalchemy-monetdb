3286a993014d088e4c388a0e494ea65467aeb9a6
Gijs Molenaar <gijs@pythonic.nl>
2017-08-23 10:42:17 +0200

    initial modernisation cleanup work

--- test/initdb.sh
+++ test/initdb.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/bin/bash -ve
 
 DATABASE="test"
-monetdb stop $DATABASE
-monetdb destroy $DATABASE
+FARM="/tmp/monetdb"
+USER="monetdb"
+PASSWORD=${USER}
+
+echo -en "user=${USER}\npassword=${PASSWORD}\n" > ~/.monetdb
+
+monetdbd stop ${FARM} || true
+monetdbd create ${FARM} || true
+monetdbd start ${FARM}
+
+monetdb stop $DATABASE || true
+monetdb destroy -f $DATABASE || true
 monetdb create $DATABASE
 monetdb release $DATABASE
-echo "create schema test_schema;" | mclient $DATABASE
-echo "create schema test_schema2;" | mclient $DATABASE
-echo "alter user monetdb set schema test_schema2;" | mclient $DATABASE
+echo "create schema test_schema;" | mclient -d $DATABASE
+echo "create schema test_schema2;" | mclient -d $DATABASE
+echo "alter user monetdb set schema test_schema2;" | mclient -d $DATABASE
 
