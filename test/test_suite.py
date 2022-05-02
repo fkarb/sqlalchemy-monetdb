8d29bd516d038badad042bba9ae414adf67c5037
MitchellWeg <mitchell.w@live.nl>
2022-05-02 15:19:29 +0200

    turn off problematic round trip tests

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,7 +1,14 @@
 import pytest
 from sqlalchemy.testing.suite import *
 from sqlalchemy.testing.suite.test_ddl import FutureTableDDLTest, TableDDLTest
+from sqlalchemy.testing.suite.test_dialect import DifficultParametersTest
 from sqlalchemy.testing.suite.test_select import FetchLimitOffsetTest
+
+class DifficultParametersTest(DifficultParametersTest):
+    @pytest.mark.skip(reason="Monet does not support identifiers with an '%' in them.")
+    def test_round_trip(*args, **kwargs):
+        pass
+
 
 class TableDDLTest(TableDDLTest):
     @pytest.mark.skip(reason="column names of level >= 3")
