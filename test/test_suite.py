0aeb18a7b20880420d81fed3dcf6de9ff18f63db
MitchellWeg <mitchell.w@live.nl>
2022-05-03 10:48:48 +0200

    turn off IsOrIsNotDistinctFrom test

--- test/test_suite.py
+++ test/test_suite.py
@@ -2,13 +2,17 @@ import pytest
 from sqlalchemy.testing.suite import *
 from sqlalchemy.testing.suite.test_ddl import FutureTableDDLTest, TableDDLTest
 from sqlalchemy.testing.suite.test_dialect import DifficultParametersTest
-from sqlalchemy.testing.suite.test_select import FetchLimitOffsetTest
+from sqlalchemy.testing.suite.test_select import FetchLimitOffsetTest, IsOrIsNotDistinctFromTest
 
 class DifficultParametersTest(DifficultParametersTest):
     @pytest.mark.skip(reason="Monet does not support identifiers with an '%' in them.")
     def test_round_trip(*args, **kwargs):
         pass
 
+class IsOrIsNotDistinctFromTest(IsOrIsNotDistinctFromTest):
+    @pytest.mark.skip(reason="Monet does not support the 'IS DISTINCT FROM' syntax.")
+    def test_is_or_is_not_distinct_from():
+        pass
 
 class TableDDLTest(TableDDLTest):
     @pytest.mark.skip(reason="column names of level >= 3")
